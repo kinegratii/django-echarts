8529f6cb7a4a8e668d53f86fedd907ca038eed43
kinegratii <kinegratii@gmail.com>
2017-09-01 11:48:34 +0800

    Update

--- django_echarts/utils.py
+++ django_echarts/utils.py
@@ -5,15 +5,26 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.utils.functional import SimpleLazyObject
 
+# Default settings for django-echarts app
 DEFAULT_SETTINGS = {
     'js_host': 'https://chfw.github.io/jupyter-echarts/echarts'
 }
 
 
+class AttrDict(dict):
+    """Add attribute access for a dict
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
 def get_django_echarts_settings():
     custom_settings = getattr(settings, 'DJANGO_ECHARTS', {})
     project_settings = custom_settings.update(DEFAULT_SETTINGS)
-    return project_settings
+    return AttrDict(project_settings)
 
 
 DJANGO_ECHARTS_SETTING = SimpleLazyObject(get_django_echarts_settings)
