cc5b78e0436db66ef9a19b4035deaad146bf35a5
kinegratii <kinegratii@gmail.com>
2018-01-03 09:37:43 +0800

    release v0.2.2

--- django_echarts/__init__.py
+++ django_echarts/__init__.py
@@ -2,5 +2,5 @@
 A django app for Echarts integration with pyecharts as chart builder.
 """
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'kinegratii'
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ packages = [
 
 setup(
     name='django-echarts',
-    version='0.2.1',
+    version='0.2.2',
     packages=packages,
     url='https://github.com/kinegratii/django-echarts',
     install_requires=['pyecharts', 'Django', 'pluck'],
