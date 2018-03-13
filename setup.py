b3486681ccea68d36ee76aaf90900629e17a8775
kinegratii <kinegratii@gmail.com>
2018-03-13 21:02:27 +0800

    Release v0.3.2

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 # coding=utf8
 
-
+import re
 from setuptools import setup, find_packages
+from pathlib import Path
+
+
+def retrieve_version():
+    p = Path('django_echarts', '__init__.py')
+    with p.open(encoding='utf8') as f:
+        version_file_content = f.read()
+        version_match = re.findall(r"__version__\s=\s'([\d.]+)'", version_file_content)
+        if version_match:
+            return version_match[0]
+        return RuntimeError("No version retrieved in package file.")
+
 
 lib_classifiers = [
     "Development Status :: 4 - Beta",
@@ -20,7 +32,7 @@ lib_classifiers = [
 
 setup(
     name='django-echarts',
-    version='0.3.1',
+    version=retrieve_version(),
     packages=find_packages(exclude=['tests']),
     url='https://github.com/kinegratii/django-echarts',
     install_requires=['Django'],
