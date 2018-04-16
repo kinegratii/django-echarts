b42d29e33073a775c4c28b5dc914cbffae043a7e
kinegratii <kinegratii@gmail.com>
2018-04-16 22:53:38 +0800

    Fix #15

--- example/demo/backend_views.py
+++ example/demo/backend_views.py
@@ -30,13 +30,11 @@ class TemperatureEChartsView(EChartsBackendView):
     template_name = 'temperature_charts.html'
 
     def get_echarts_instance(self, **kwargs):
-        context = super(TemperatureEChartsView, self).get_context_data(**kwargs)
         t_data = models.TemperatureRecord.objects.all().order_by('create_time').values_list('high', 'create_time')
         hs, ds = zip(*t_data)
         line = Line('High Temperature')
         line.add('High', ds, hs)
-        context['line'] = line
-        return context
+        return line
 
 
 class PageDemoView(EChartsBackendView):
